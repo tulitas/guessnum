b561815ca9e3f5edd553bcb0d53802de9f0f439a
sergej <tulitas1@gmail.com>
2018-09-01 22:06:26 +0300

    Game with answer and continue

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -8,39 +8,44 @@ public class Main {
     static Scanner scan = new Scanner(System.in);
 
     public static void main(String[] args) {
-
-        //generiruet slu4ainoe 4islo
-        int myNum = rand.nextInt(100) + 1;
-        //esli zakomentit` sled stroku, 4islo ne vidno
-        System.out.println(myNum);
-
-        boolean userLost = true;
-        for (int i = 1; i < 11; i++) {
-            System.out.println("Popitka #" + i);
-            //sprasivaem 4islo
-            int userNum = scan.nextInt();
-            //varianti otveta
-
-            if (myNum == userNum) {
-                System.out.println(" Ugadal!");
-                userLost = false;
-                break;
-            } else if
-            (myNum < userNum) {
-                System.out.println(" myNum < userNum");
-            } else {
-                System.out.println(" myNum > userNum");
-
+        String answer;
+        //na4alo cikla, kotorij budet povtorjatsa
+        do {
+            //generiruet slu4ainoe 4islo
+            int myNum = rand.nextInt(100) + 1;
+            //esli zakomentit` sled stroku, 4islo ne vidno
+            //System.out.println(myNum);
+            boolean userLost = true;
+            //kol-vo popitok
+            for (int i = 1; i < 11; i++) {
+                System.out.println("Попытка #" + i);
+                //sprasivaem 4islo
+                int userNum = scan.nextInt();
+                //varianti otveta
+                if (myNum == userNum) {
+                    System.out.println(" Победа!");
+                    userLost = false;
+                    break;
+                } else if
+                (myNum < userNum)
+                    System.out.println(" myNum < userNum");
+                else {
+                    System.out.println(" myNum > userNum");
+                }
             }
-
-
-        }
-        if (userLost) {
-            System.out.println("Proigral!");
-        }
+            if (userLost) {
+                System.out.println("Проиграл!");
+            }
+            //vopros na prodolzenie igri
+            System.out.println(" Ещё раз? ");
+            System.out.println(" Y ? " + " N ? ");
+            answer = scan.next();
+        } while (answer.equals("Y"));
+        do {
+            System.out.println("Пока, очень жаль!");
+            break;
+        } while (answer.equals("N"));
     }
 }
-
-
 
 
